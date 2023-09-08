dcd4c652b3e9a4be8f405343f78edd940c09e936
jagveerChauhan <jagveer@onetick.in>
2023-09-08 20:16:51 +0530

    initial commit

---
+++ headers/header.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+    <a href="./" class="logo"><img src="./assets/images/logo.png" alt="DPS Electricals Company Logo" width="100px"></a>
+    <input type="checkbox" id="toggler">
+    <label for="toggler"><img src="./assets/icons/toggle.png" height="30" width="30" alt="Show navbar button" title="navbar button" loading="lazy"></label>
+    <div class="menu">
+        <ul class="list">
+            <li><a href="./">Products</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ headers/top-header.php
@@ -0,0 +1,13 @@
+<nav class="top-header">
+    <span>
+        <img src="./assets/icons/telephone.png" alt="telephone icon" title="telephone" loading="lazy" height="12" width="12"> <a href="tel:9818520700">&nbsp;(+91) 9818520700</a>
+    </span>
+    <span><img id="gmail-icon" src="./assets/icons/gmail.png" alt="gmail icon" title="gmail" loading="lazy" height="15" width="15"><a href="mailto:Dpselectricals@gmail.com">&nbsp; &nbsp;Dpselectricals@gmail.com</a></span>
+    <div class="menu">
+        <ul class="list">
+            <li><a href="#"><img src="./assets/icons/linkedin.png" alt="linkedin icon" title="linkedin" loading="lazy" height="15" width="15"></a></li>
+            <li><a href="https://www.facebook.com/profile.php?id=100052344243891"><img src="./assets/icons/facebook.png" alt="facebook icon" title="facebook" loading="lazy" height="15" width="15"></a></li>
+            <li><a href="https://www.instagram.com/dpselectricalsgmail.com1/"><img src="./assets/icons/instagram.png" alt="instagram icon" title="instagram" loading="lazy" height="15" width="15"></a></li>
+        </ul>
+    </div>
+</nav>
